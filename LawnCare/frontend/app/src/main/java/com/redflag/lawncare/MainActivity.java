6b9796469b247005847bee5d01426f759f9117d3
Ryan Moore <wrm192@gmail.com>
2020-03-24 17:09:05 -0400

    removed old button

--- LawnCare/frontend/app/src/main/java/com/redflag/lawncare/MainActivity.java
+++ LawnCare/frontend/app/src/main/java/com/redflag/lawncare/MainActivity.java
@@ -35,7 +35,6 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
 
         setSupportActionBar(toolbar);
-        textView1 = findViewById(R.id.textView1);
 
        FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
