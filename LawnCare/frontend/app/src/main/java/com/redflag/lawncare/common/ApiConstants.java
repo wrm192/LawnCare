3e5dc9101f6bc917f952ab19f1a52805725f543d
Ryan Moore <wrm192@gmail.com>
2020-03-24 16:43:19 -0400

    Removed old API

--- LawnCare/frontend/app/src/main/java/com/redflag/lawncare/common/ApiConstants.java
+++ LawnCare/frontend/app/src/main/java/com/redflag/lawncare/common/ApiConstants.java
@@ -1,10 +1,7 @@
 package com.redflag.lawncare.common;
 
 public class ApiConstants {
-
-    public static final String API_AUTH = "https://cdbefccab875a81af90a633589bc776e:b9cfc6fa6e63b0372898af0510628134"; //"https://API_KEY:API_PW"
     public static final String API_BASE = "https://ezmow-ca.myshopify.com/admin/api/";
     public static final String API_VER = "2020-01/";
     public static final String FULL_BASE = API_BASE + API_VER;
-
 }
