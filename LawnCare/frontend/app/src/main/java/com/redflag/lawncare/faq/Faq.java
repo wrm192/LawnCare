95d4b9e87d7f46fece0564903a826640f7d10c78
Nicolas Smeall <nic_smeall@live.com>
2020-03-27 20:21:49 -0400

    Added FAQ 1.0

---
+++ LawnCare/frontend/app/src/main/java/com/redflag/lawncare/faq/Faq.java
@@ -0,0 +1,19 @@
+package com.redflag.lawncare.faq;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+import com.redflag.lawncare.R;
+
+import com.redflag.lawncare.MainActivity;
+
+public class temp extends AppCompatActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.faq);
+
+    }
+}
