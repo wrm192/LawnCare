cfa710a04713b8ce9f6b0ec48efc26fa44e46049
jjyhj <56215488+jjyhj@users.noreply.github.com>
2020-03-30 00:03:42 -0400

    fixed the fucking C and again

--- LawnCare/frontend/app/src/main/java/com/redflag/lawncare/common/TabsApadater.java
+++ LawnCare/frontend/app/src/main/java/com/redflag/lawncare/common/TabsApadater.java
@@ -5,7 +5,7 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentStatePagerAdapter;
 
-import com.redflag.lawncare.Contact.ContactFragment;
+import com.redflag.lawncare.contact.ContactFragment;
 import com.redflag.lawncare.book.BookNowFragment;
 
 public class TabsApadater extends FragmentStatePagerAdapter {
