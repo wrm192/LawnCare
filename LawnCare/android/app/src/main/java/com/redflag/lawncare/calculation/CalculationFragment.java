7a80c035c8561ae4829380d4d3e1cec066d7eecc
Yue Gao <gaoyue.218@gmail.com>
2020-10-04 15:30:25 -0400

    error

--- LawnCare/android/app/src/main/java/com/redflag/lawncare/calculation/CalculationFragment.java
+++ LawnCare/android/app/src/main/java/com/redflag/lawncare/calculation/CalculationFragment.java
@@ -44,7 +44,6 @@ public class CalculationFragment extends Fragment {
         });
 
         return view;
-
     }
          private boolean checkForError(EditText text, boolean errorState) {
             if("".equals(text.getText().toString())){
