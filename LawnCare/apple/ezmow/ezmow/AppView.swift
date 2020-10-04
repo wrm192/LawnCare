8516a68481bc394a494ca1ebb30e53591d5c5d22
jjyhj <56215488+jjyhj@users.noreply.github.com>
2020-10-04 15:26:08 -0400

    tabview working
    Please enter the commit message for your changes. Lines starting

---
+++ LawnCare/apple/ezmow/ezmow/AppView.swift
@@ -0,0 +1,20 @@
+//
+//  AppView.swift
+//  ezmow
+//
+//  Created by Jarrod Jung on 2020-10-04.
+//
+
+import SwiftUI
+
+struct AppView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AppView_Previews: PreviewProvider {
+    static var previews: some View {
+        AppView()
+    }
+}
