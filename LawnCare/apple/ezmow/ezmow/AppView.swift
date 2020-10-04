a71adaf6ada9544321dc6718087e2bad25d59172
jjyhj <56215488+jjyhj@users.noreply.github.com>
2020-10-04 15:56:09 -0400

    recommit tabview

--- LawnCare/apple/ezmow/ezmow/AppView.swift
+++ LawnCare/apple/ezmow/ezmow/AppView.swift
@@ -9,12 +9,24 @@ import SwiftUI
 
 struct AppView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        TabView {
+            ContentView().tabItem {Image(systemName: "list.dash")
+                Text("Menu")
+            }
+            
+            OrderView().tabItem {
+                Image(systemName: "square.and.pencil")
+                Text("Order")
+            }
+        }
+       // Text("Hello, world!")
+       //     .padding()
     }
 }
 
-struct AppView_Previews: PreviewProvider {
+struct ContentView_Previews: PreviewProvider {
+    static let order = Order()
     static var previews: some View {
-        AppView()
+        AppView().environmentObject(order)
     }
 }
