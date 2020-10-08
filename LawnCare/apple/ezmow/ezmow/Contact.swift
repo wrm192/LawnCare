794992a0a817cc5bfee9ee92d885d385e1748483
jjyhj <56215488+jjyhj@users.noreply.github.com>
2020-10-08 17:01:55 -0400

    tabbiew minor fixues + homebutton. 0 should be 0 issues now

---
+++ LawnCare/apple/ezmow/ezmow/Contact.swift
@@ -0,0 +1,20 @@
+//
+//  Contact.swift
+//  ezmow
+//
+//  Created by Jarrod Jung on 2020-10-08.
+//
+
+import SwiftUI
+
+struct Contact: View {
+    var body: some View {
+        Text("Contact")
+    }
+}
+
+struct Contact_Previews: PreviewProvider {
+    static var previews: some View {
+        Contact()
+    }
+}
