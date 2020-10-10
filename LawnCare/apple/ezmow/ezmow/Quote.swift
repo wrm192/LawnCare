70d56c01fadb420e9e8394ea48da24126683ad19
jjyhj <56215488+jjyhj@users.noreply.github.com>
2020-10-09 20:42:40 -0400

    UI for Quote page made. no math being done on button push at this time. need a place to display math at some point.

--- LawnCare/apple/ezmow/ezmow/Quote.swift
+++ LawnCare/apple/ezmow/ezmow/Quote.swift
@@ -8,8 +8,40 @@
 import SwiftUI
 
 struct Quote: View {
+    @State var area = ""
+    @State var obstructions = ""
+    
     var body: some View {
-        Text("FAQ")
+        
+        
+        //if keybaord doesnt come up in sim. shift, cmd, k
+        NavigationView{
+            Form {
+
+                Section (header: Text("Area")){
+                    TextField("Area", text:
+                              $area)
+                        .keyboardType(.decimalPad)
+                }
+                Section (header: Text("Obstructions")){
+                    TextField("Obstructions", text:
+                              $obstructions)
+                        .keyboardType(.decimalPad)
+                }
+
+                
+                Section {
+                    Button(action : {print("Quote button psuhed")}, label: {
+                        Text("Get Quote").bold().foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+                            .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .center)
+                    })
+                }
+        
+            }
+            .navigationTitle("Quote")
+
+        }
+
     }
 }
 
