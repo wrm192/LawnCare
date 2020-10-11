9b018b2edfd6ca421fb00bb369454041f7bb0677
jjyhj <56215488+jjyhj@users.noreply.github.com>
2020-10-10 23:45:52 -0400

    home tab complete

--- LawnCare/apple/ezmow/ezmow/HomeView.swift
+++ LawnCare/apple/ezmow/ezmow/HomeView.swift
@@ -8,12 +8,33 @@
 import SwiftUI
 
 struct HomeView: View {
+
     var body: some View {
-        Button(action: {
+        
+        //if keybaord doesnt come up in sim. shift, cmd, k
+        NavigationView{
+            Form {
+                Section (header: Text("\nEZMow is lawn care made easy, booka service with us and have a lawn mowing robot come to your door steps to take care of the rest! Use the contact us form for any inquiries. \n\n **Employee supervisor will be on site")) {}
+                
+                Image("Logo")
+                    .resizable()
+                    .scaledToFit()
+                
+                
+                /*Text ("EZMow is lawn care made easy, booka service with us and have a lawn mowing robot come to your door steps to take care of the rest! Use the contact us form for any inquiries. \n\n **Employee supervisor will be on site")
+                    .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .center)*/
+
+        
+            }
+            .navigationTitle("About Us!")
+
+        }
+
+       /* Button(action: {
             print("Home Button Pushed")
         }) {
             Image ("HomeButton");
-        }
+        }*/
     }
 }
 
