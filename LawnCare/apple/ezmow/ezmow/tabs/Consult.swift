716e0ffb0b3511321080015429fa0d7d67b82e7d
jjyhj <56215488+jjyhj@users.noreply.github.com>
2020-10-21 15:38:00 -0400

    initializing

--- LawnCare/apple/ezmow/ezmow/tabs/Consult.swift
+++ LawnCare/apple/ezmow/ezmow/tabs/Consult.swift
@@ -8,8 +8,42 @@
 import SwiftUI
 
 struct Consult: View {
+    @State var name = ""
+    @State var phoneNumb = ""
+    @State var address = ""
+    
     var body: some View {
-        Text("Consult")
+        //if keybaord doesnt come up in sim. shift, cmd, k
+        NavigationView{
+            Form {
+
+                Section (header: Text("Name")){
+                    TextField("Name", text:
+                              $name)
+                }
+                
+                Section (header: Text("Phone Number")){
+                    TextField("Phone Number", text:
+                              $phoneNumb)
+                        .keyboardType(.decimalPad)
+                }
+
+                Section (header: Text("Addreess")){
+                    TextField("Address", text:
+                              $address)
+                }
+                
+                Section {
+                    Button(action : {print("Quote button psuhed")}, label: {
+                        Text("Book Now").bold().foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+                            .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .center)
+                    })
+                }
+        
+            }
+            .navigationTitle("Book Now")
+
+        }
     }
 }
 
@@ -18,3 +52,4 @@ struct Consult_Previews: PreviewProvider {
         Consult()
     }
 }
+
