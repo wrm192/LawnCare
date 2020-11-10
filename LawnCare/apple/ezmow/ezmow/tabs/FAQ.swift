b52d0c3af32aba47e43e1abb92743609c2e2533c
Nicolas Smeall <nic_smeall@live.com>
2020-11-09 22:54:41 -0500

    Added french text, FAQ page.

--- LawnCare/apple/ezmow/ezmow/tabs/FAQ.swift
+++ LawnCare/apple/ezmow/ezmow/tabs/FAQ.swift
@@ -2,16 +2,49 @@
 //  FAQ.swift
 //  ezmow
 //
-//  Created by Jarrod Jung on 2020-10-08.
+//  Created by Nicolas Smeall on 2020-10-08.
 //
 
 import SwiftUI
 
 struct FAQ: View {
     var body: some View {
-        Text(NSLocalizedString("faq", comment: ""))
+        NavigationView {
+            List {
+                
+                Section (header: Text(NSLocalizedString("question1", comment: "")).bold().font(.system(size:22)))
+                    {}
+                Section (header: Text(NSLocalizedString("answer1", comment: "")).bold().font(.system(size:16)).foregroundColor(.red)){
+                }
+                
+                Section (header: Text(NSLocalizedString("question2", comment: "")).bold().font(.system(size:22))){
+                }
+                Section (header: Text(NSLocalizedString("answer2", comment: "")).bold().font(.system(size:16)).foregroundColor(.red)){
+                }
+                Section (header: Text(NSLocalizedString("question3", comment: "")).bold().font(.system(size:22))){
+                }
+                Section (header: Text(NSLocalizedString("answer3", comment: "")).bold().font(.system(size:16)).foregroundColor(.red)){
+                }
+                Section (header: Text(NSLocalizedString("question4", comment: "")).bold().font(.system(size:22))){
+                }
+                Section (header: Text(NSLocalizedString("answer4", comment: "")).bold().font(.system(size:16)).foregroundColor(.red)){
+                }
+                Image("Logo")
+                    .resizable()
+                    .scaledToFit().offset(x: /*@START_MENU_TOKEN@*/10.0/*@END_MENU_TOKEN@*/, y:50)
+            } .navigationBarTitle(NSLocalizedString("faq", comment: ""))
+            
+        }
+        
+        
     }
+    
+    func buttonAction() {
+        print("FAQ button psuhed")
+    }
+    
 }
+
 
 struct FAQ_Previews: PreviewProvider {
     static var previews: some View {
