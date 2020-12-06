dc4948b54068776894cd06277dd38cc1552ead5a
Ryan Moore <wrm192@gmail.com>
2020-12-06 15:48:42 -0500

    quote validation

--- LawnCare/apple/ezmow/ezmow/tabs/Quote.swift
+++ LawnCare/apple/ezmow/ezmow/tabs/Quote.swift
@@ -9,6 +9,12 @@ import SwiftUI
 import ToastUI
 import Combine
 
+extension String {
+    var isInt: Bool {
+        return Int(self) != nil
+    }
+}
+
 struct Quote: View {
     @State var area = ""
     @State var obstructions = ""
@@ -35,10 +41,18 @@ struct Quote: View {
 
                 
                 Section {
-                    Button(action : {buttonAction()}, label: {
-                        Text(NSLocalizedString("quoteGetQuoteButton", comment: "")).bold().foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
-                            .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .center)
-                    })
+                    if (area.isInt && obstructions.isInt) {
+                        Button(action : {buttonAction()}, label: {
+                            Text(NSLocalizedString("quoteGetQuoteButton", comment: "")).bold().foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+                                .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .center)
+                        })
+                    } else {
+                        Button(action : {buttonAction()}, label: {
+                            Text(NSLocalizedString("quoteGetQuoteButton", comment: "")).bold().foregroundColor(.init(red: 0.31, green: 0.34, blue: 0.36))
+                                .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .center)
+                        })
+                    }
+                    
                 }.toast(isPresented: $presentingToast, dismissAfter: 2.0) {
                     print("Toast dismissed")
                   } content: {
