f4a8c31ef390657867d6f1024a7c7764360b14eb
jjyhj <56215488+jjyhj@users.noreply.github.com>
2020-12-06 16:05:18 -0500

    consult unit testing

--- LawnCare/apple/ezmow/ezmowTests/ezmowTests.swift
+++ LawnCare/apple/ezmow/ezmowTests/ezmowTests.swift
@@ -12,10 +12,12 @@ class ezmowTests: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        super.setUp()
     }
 
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
     }
 
     func testExample() throws {
